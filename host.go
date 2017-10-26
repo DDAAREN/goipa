a733f8f5e5f34baf5477eba629051646a9d02516
pt <ddaaren@163.com>
2017-10-26 10:47:25 +0800

    add function "ipa host-mod --sshpubkey"

--- host.go
+++ host.go
@@ -31,3 +31,29 @@ func (c *Client) HostShow(fqdn string) (*HostRecord, error) {
 
 	return &hostRec, nil
 }
+
+
+type HostPubKey struct {
+    Dn          string      `json:"dn"`
+    Fqdn        []string    `json:"fqdn"`
+    //SshPubKey   []map[string]string    `json:"ipasshpubkey"`
+    SshPubKey   []interface{}    `json:"ipasshpubkey"`
+}
+
+func (c *Client) HostPubKey(fqdn string, sshpubkeys []string) (*HostPubKey ,error) {
+    options := map[string]interface{}{
+        "ipasshpubkey": sshpubkeys }
+
+    res, err := c.rpc("host_mod", []string{fqdn}, options)
+    if err != nil {
+        return nil, err
+    }
+        
+    var hostPubKey HostPubKey
+    err = json.Unmarshal(res.Result.Data, &hostPubKey)
+    if err != nil {
+        return nil, err
+    }
+
+    return &hostPubKey, nil
+}
